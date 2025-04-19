e9539cdedd3b2e56a7a0d91317c17096a7dd68e4
Anderson <Andersonfrfilho@gmail.com>
2025-04-19 00:14:18 -0300

    feat: import module health

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import envValidation from './config/env.validation';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './modules/user/user.module';
+import { HealthModule } from './modules/health/health.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validationSchema: envValidation }),
+    HealthModule,
     UserModule,
   ],
   controllers: [AppController],
