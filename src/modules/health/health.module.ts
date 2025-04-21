3d53690b3cdff967c7fd1825e40c2575aa2c87b7
Anderson <Andersonfrfilho@gmail.com>
2025-04-21 11:20:40 -0300

    feat: add health module

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { HealthCheckService } from './health.service';
+import { LogModule } from 'src/providers/log/log.module';
 @Module({
+  imports: [LogModule],
   controllers: [HealthController],
   providers: [HealthCheckService],
   exports: [HealthCheckService],
