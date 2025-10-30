2bd95fea733105eccbc581744733c57653e15adc
Anderson <andersonfrfilho@gmail.com>
2025-10-30 00:57:38 -0300

    feat: add enviroment in swagger config

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -4,8 +4,8 @@ export const swaggerConfig = new DocumentBuilder()
   .setTitle('Backend Service API')
   .setDescription('Backend Service API Documentation Template NestJs Fastify')
   .setVersion('1.0')
-  .addBearerAuth() // Se usar autenticação JWT
-  .addServer(process.env.BASE_URL_DEV || 'http://localhost:3333', 'Development') // Default: dev local
+  .addBearerAuth()
+  .addServer(process.env.BASE_URL_DEV || 'http://localhost:3333', 'Development')
   .addServer(
     process.env.BASE_URL_HML || 'https://api-hml.example.com',
     'Staging (STG)',
