1e31697cfdd8f05ffa97324cf2bf6de9763782d8
Anderson <andersonfrfilho@gmail.com>
2025-12-16 23:46:52 -0300

    test: correct e2e testing

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -3,17 +3,7 @@ import 'dotenv/config';
 import { ConfigErrorFactory } from '@modules/error/application/factories';
 
 import envValidationSchema from './env.validation';
-
-export interface DatabaseConfigValues {
-  host: string;
-  port: number;
-  username: string;
-  password: string;
-  database: string;
-  timezone: string;
-  logging: boolean;
-  synchronize: boolean;
-}
+import { DatabaseConfigValues } from './types';
 
 export function getDatabaseConfig(): DatabaseConfigValues {
   const { error, value } = envValidationSchema.validate(process.env, {
@@ -35,7 +25,30 @@ export function getDatabaseConfig(): DatabaseConfigValues {
     DATABASE_POSTGRES_TIMEZONE,
     DATABASE_POSTGRES_LOGGING,
     DATABASE_POSTGRES_SYNCHRONIZE,
+
+    DATABASE_POSTGRES_TEST_E2E_PORT,
+    DATABASE_POSTGRES_TEST_E2E_NAME,
+    DATABASE_POSTGRES_TEST_E2E_HOST,
+    DATABASE_POSTGRES_TEST_E2E_USER,
+    DATABASE_POSTGRES_TEST_E2E_PASSWORD,
+    DATABASE_POSTGRES_TEST_E2E_SYNCHRONIZE,
+    DATABASE_POSTGRES_TEST_E2E_LOGGING,
+    DATABASE_POSTGRES_TEST_E2E_TIMEZONE,
   } = value;
+  const isE2E = process.env.NODE_ENV === 'test' || process.env.JEST_WORKER_ID !== undefined;
+
+  if (isE2E) {
+    return {
+      host: DATABASE_POSTGRES_TEST_E2E_HOST,
+      port: DATABASE_POSTGRES_TEST_E2E_PORT,
+      username: DATABASE_POSTGRES_TEST_E2E_USER,
+      password: DATABASE_POSTGRES_TEST_E2E_PASSWORD,
+      database: DATABASE_POSTGRES_TEST_E2E_NAME,
+      timezone: DATABASE_POSTGRES_TEST_E2E_TIMEZONE,
+      logging: DATABASE_POSTGRES_TEST_E2E_LOGGING ?? false,
+      synchronize: DATABASE_POSTGRES_TEST_E2E_SYNCHRONIZE ?? false,
+    };
+  }
 
   return {
     host: DATABASE_POSTGRES_HOST,
---
+++ src/config/types.ts
@@ -0,0 +1,10 @@
+export interface DatabaseConfigValues {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+  timezone: string;
+  logging: boolean;
+  synchronize: boolean;
+}
